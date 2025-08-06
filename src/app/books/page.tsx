4ac1d9eeb4831ce4a963ce3b3c64b024d18831f3
Srijoy2007 <youremail@example.com>
2025-08-06 15:44:30 +0530

    test

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -249,7 +249,7 @@ export default function BookLibrary() {
  export default function UnderConstruction() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#FFEDD5] text-center text-black">
-      <h1 className="text-2xl font-bold mb-4">🚧 Under Construction</h1>
+      <h1 className="text-2xl font-bold mb-4"> Under Construction books will be updated soon </h1>
       <p className="text-sm text-gray-700">This page is currently being built. Check back soon!</p>
     </div>
   );
